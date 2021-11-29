69c787f5f43b1f90fa2fa677d2468d2f332c23f1
bardia <bardiashams1375@gmail.com>
2021-11-29 11:45:49 +0330

    formik

---
+++ src/Pages/profile/editInformation/editInformation.js
@@ -0,0 +1,175 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
+// import ButtonComponent from "../../../components/Button/button";
+import "./editInformation.css";
+const EditInformation = () => {
+  const MyTextInput2 = ({ label, ...props }) => {
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input>. We can use field meta to show an error
+    // message if the field is invalid and it has been touched (i.e. visited)
+    const [field, meta] = useField(props);
+    return (
+      <div className="formik-main-items" style={{}}>
+        {meta.touched && meta.error ? (
+          <div className="formik-items">
+            <input
+              className="text-input"
+              style={{ border: "2px solid red" }}
+              {...field}
+              {...props}
+            />
+
+            <div className="formik-error">{meta.error}</div>
+          </div>
+        ) : (
+          <div className="formik-items">
+            <input className="text-input" {...field} {...props} />
+          </div>
+        )}
+      </div>
+    );
+  };
+ const Button=(e)=>{
+     return(
+         <button type="button" className="editInformation-button">ثبت</button>
+     )
+ }
+  return (
+    <div className="editInformation-container">
+      <div className="editInformation-back">
+        <p>بازگشت</p>
+        <img src="/nahoor home page/profile/Vector.svg" />
+      </div>
+
+      <div className="editInformation-title">
+        <h2>ویرایش اطلاعات حساب کاربری</h2>
+      </div>
+
+      <div className="editInformation-main">
+        <div className="editInformation-img">
+          <img
+            className="editInformation-profileImg"
+            src="/nahoor home page/profile/profile photo.svg"
+          />
+          <div className="editInformation-changePhoto">
+            <img src="/nahoor home page/profile/Group 422.svg" />
+            <h3>تغییر عکس پروفایل</h3>
+          </div>
+        </div>
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            story: "",
+            cellPhone: "",
+            NationalCode: "",
+            iban: "",
+            cartNumber: "",
+          }}
+          validationSchema={Yup.object({
+            firstName: Yup.string()
+              .max(25, "شما مجاز به استفاده از حداکثر 15 کاراکتر هستید")
+              .required("لطفا فیلد را کامل کنید"),
+            lastName: Yup.string()
+              .max(25, "شما مجاز به استفاده از حداکثر 15 کاراکتر هستید")
+              .required("لطفا فیلد را کامل کنید"),
+            cellPhone: Yup.string()
+              .max(15, "شما مجاز به استفاده از حداکثر 15 کاراکتر هستید")
+              .required("لطفا فیلد را کامل کنید"),
+            NationalCode: Yup.string()
+              .max(8, "شما مجاز به استفاده از حداکثر 10 کاراکتر هستید")
+              .required("لطفا فیلد را کامل کنید"),
+            email: Yup.string()
+              .email("لطفا ایمیل را درست وارد کنید")
+              .required("لطفا فیلد را کامل کنید"),
+            iban: Yup.string()
+            .max(22, "شما مجاز به استفاده از حداکثر 24 کاراکتر هستید")
+            .required("لطفا فیلد را کامل کنید"),
+            cartNumber: Yup.string()
+            .max(16, "شما مجاز به استفاده از حداکثر 16 کاراکتر هستید")
+            .required("لطفا فیلد را کامل کنید"),
+            story: Yup.string().required("لطفا فیلد را کامل کنید"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 100);
+          }}
+        >
+          <Form className="editInformation-content">
+            <div>
+              <h2>اطلاعات شخصی</h2>
+            </div>
+            <div className="editInformation-items">
+              <MyTextInput2
+                className="editInformation-item"
+                name="firstName"
+                type="text"
+                placeholder="نام"
+              />
+              <MyTextInput2
+                className="editInformation-item"
+                name="lastName"
+                type="text"
+                placeholder=" نام خانوادگی"
+              />
+              <MyTextInput2
+                className="editInformation-item"
+                name="cellPhone"
+                type="number"
+                placeholder="شماره تلفن همراه"
+              />
+              <MyTextInput2
+                className="editInformation-item"
+                name="NationalCode"
+                type="number"
+                placeholder="کدملی"
+              />
+              <MyTextInput2
+                className="editInformation-item"
+                label="ایمیل"
+                name="email"
+                type="email"
+                placeholder="ایمیل"
+              />
+            </div>
+            <div>
+              <h2>اطلاعات حساب</h2>
+            </div>
+            <div>
+              <MyTextInput2
+                className="editInformation-item"
+                name="iban"
+                type="number"
+                placeholder="شماره شبا"
+              />
+              <MyTextInput2
+                className="editInformation-item"
+                name="cartNumber"
+                type="number"
+                placeholder="شماره کارت"
+              />
+            </div>
+            <div>
+              <Button  onSubmit />
+              <button type="submit" className="editInformation-button" >ثبت</button>
+            </div>
+            {/* <div className="formikmain-bottom">
+              <ButtonComponent
+                onSubmit
+                className="formik-button"
+                title="ارسال نظر"
+              />
+            </div> */}
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default EditInformation;
--- src/Pages/profile/profile.js
+++ src/Pages/profile/profile.js
@@ -4,6 +4,7 @@ import "./profile.css";
 import Call from "./call/call";
 import ConditionNahor from "./conditionNahor/ConditionNahor";
 import Information from "./information/information";
+import EditInformation from "./editInformation/editInformation";
 
 const Profile = () => {
   const sideBarItems = [
@@ -30,99 +31,101 @@ const Profile = () => {
   ];
   return (
     <div className="profile-container">
-      {sideBarItems.map((sideBarItem) => {
-        return (
-          <div className="profile-sideBar">
-            <div className="profile-photo">
-              <img src="/nahoor home page/profile/profile photo.svg" />
-              <h3>{sideBarItem.profileName}</h3>
-            </div>
-
-            <div className="profile-sideBarItem1">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/اطلاعات کاربر.png"
-                  alt=""
-                  width="100%"
-                />
+      <div>
+        {sideBarItems.map((sideBarItem) => {
+          return (
+            <div className="profile-sideBar">
+              <div className="profile-photo">
+                <img src="/nahoor home page/profile/profile photo.svg" />
+                <h3>{sideBarItem.profileName}</h3>
               </div>
-              <p>{sideBarItem.information}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/Group 62.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem1">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/اطلاعات کاربر.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.information}</p>
               </div>
-              <p>{sideBarItem.order}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/تماس با ما.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/Group 62.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.order}</p>
               </div>
-              <p>{sideBarItem.call}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/about us.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/تماس با ما.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.call}</p>
               </div>
-              <p>{sideBarItem.aboutUs}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/supprt.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/about us.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.aboutUs}</p>
               </div>
-              <p>{sideBarItem.support}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/قوانین و مقررات.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/supprt.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.support}</p>
               </div>
-              <p>{sideBarItem.roll}</p>
-            </div>
 
-            <div className="profile-sideBarItem">
-              <div style={{ width: "8%", marginLeft: "8px" }}>
-                <img
-                  src="/nahoor home page/profile/Icons/Icon/non active/خروج.png"
-                  alt=""
-                  width="100%"
-                />
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/قوانین و مقررات.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.roll}</p>
               </div>
-              <p>{sideBarItem.exite}</p>
-            </div>
-          </div>
-        );
-      })}
 
+              <div className="profile-sideBarItem">
+                <div style={{ width: "8%", marginLeft: "8px" }}>
+                  <img
+                    src="/nahoor home page/profile/Icons/Icon/non active/خروج.png"
+                    alt=""
+                    width="100%"
+                  />
+                </div>
+                <p>{sideBarItem.exite}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <div className="profile-components">
         {/* <Support/> */}
         {/* <Call/> */}
-          {/* <ConditionNahor/> */}
-          <Information/>
+        {/* <ConditionNahor/> */}
+        {/* <Information/> */}
+        <EditInformation />
       </div>
     </div>
   );
